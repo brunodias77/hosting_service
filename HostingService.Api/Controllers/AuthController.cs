db44a4dfb2fe739bd92efd9b245164664d8d0a4b
brunodias77 <brunohdias95@gmail.com>
2023-10-24 19:42:38 -0300

    refact

--- HostingService.Api/Controllers/AuthController.cs
+++ HostingService.Api/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using HostingService.Api.Models;
+using HostingService.Application.Users.LoginUser;
 using HostingService.Application.Users.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -14,8 +15,15 @@ namespace HostingService.Api.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginModel model)
+        public async Task<IActionResult> Login([FromBody] LoginUserCommand request)
         {
+            var response = await _mediator.Send(new LoginUserCommand(request.Username, request.Password));
+            if (response.SignInResult.Succeeded)
+            {
+                //    return Ok(new { Token = await _repository.CreateTokenAsync(user) });
+                return Ok();
+            }
+
             //Console.WriteLine(model);
             //var user = await _userManager.FindByNameAsync(model.Username);
             //var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, lockoutOnFailure: false);
@@ -27,15 +35,36 @@ namespace HostingService.Api.Controllers
             //{
             //    return Unauthorized(new { message = "Nome de usuário ou senha incorretos." });
             //}
-            return Ok();
+            return Unauthorized();
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterUserCommand request)
         {
-            // teste
+            //if (!ModelState.IsValid) return BadRequest("Sua mensagem de erro aqui");
+
+            //// teste
+            //var response = await _mediator.Send(new RegisterUserCommand(request.FirstName, request.LastName, request.Email, request.Password));
+            //if (!response.Succeeded)
+            //{
+            //    return StatusCode(StatusCodes.Status500InternalServerError, new AuthResponse { Status = "Error", Message = "Nao foi possivel criar o usuario " + response.Errors.FirstOrDefault().Description });
+            //}
+            //return Ok(new AuthResponse { Status = "Success", Message = "Usuario criado com sucesso !" });
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return BadRequest(new { Status = "Error", Message = "Erro de validação", Errors = errors });
+            }
+
             var response = await _mediator.Send(new RegisterUserCommand(request.FirstName, request.LastName, request.Email, request.Password));
-            return Ok(response);
+            if (!response.Result.Succeeded)
+            {
+                var errorDescription = response.Result.Errors.FirstOrDefault()?.Description ?? "Erro desconhecido";
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "Não foi possível criar o usuário", Error = errorDescription });
+            }
+
+            return Ok(new AuthResponse { Status = "Success", Message = "Usuário criado com sucesso!" });
         }
     }
 }
